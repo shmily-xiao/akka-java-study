518cb2bb6d9db573307af9be53a76ba9ca8a3e1d
shmily <wangzaijun1234@126.com>
2021-02-25 23:38:34 +0800

    [add] first commit: demo

--- src/main/java/com.study.akka/Greeter.java
+++ src/main/java/com.study.akka/Greeter.java
@@ -1,11 +1,45 @@
 package com.study.akka;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
 
 public class Greeter extends AbstractActor {
+    static public Props props(String message, ActorRef printerActor) {
+        return Props.create(Greeter.class, () -> new Greeter(message, printerActor));
+    }
+
+    static public class WhoToGreet {
+        public final String who;
+
+        public WhoToGreet(String who) {
+            this.who = who;
+        }
+    }
+
+    static public class Greet {
+        public Greet() {
+        }
+    }
+
+    private final String message;
+    private final ActorRef printerActor;
+    private String greeting = "";
+
+    public Greeter(String message, ActorRef printerActor) {
+        this.message = message;
+        this.printerActor = printerActor;
+    }
 
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match(WhoToGreet.class, wtg -> {
+                    this.greeting = message + ", " + wtg.who;
+                })
+                .match(Greet.class, x -> {
+                    printerActor.tell(new Printer.Greeting(greeting), getSelf());
+                })
+                .build();
     }
 }
