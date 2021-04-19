9dc110d299d742e2e40578e4fe46bdcc01bfbb83
xifeng <zaijun.wzj@alibaba-inc.com>
2021-04-19 09:59:50 +0800

    [add] actor 依赖注入 完结，但是这个章节的代码没有办法跑

---
+++ src/main/java/com/study/akka/othertest/DependencyInjector.java
@@ -0,0 +1,38 @@
+package com.study.akka.othertest;
+
+import akka.actor.Actor;
+import akka.actor.ActorRef;
+import akka.actor.IndirectActorProducer;
+import akka.actor.Props;
+
+/**
+ * 依赖注入
+ * @author wzj
+ * @date 2021/04/19
+ */
+public class DependencyInjector implements IndirectActorProducer {
+
+    private final Object applicationContext;
+
+    private final String beanName;
+
+    public DependencyInjector(Object applicationContext, String beanName) {
+        this.applicationContext = applicationContext;
+        this.beanName = beanName;
+    }
+
+    @Override
+    public Actor produce() {
+        TheActor result;
+        result = new TheActor( (String) applicationContext);
+        return result;
+    }
+
+    @Override
+    public Class<? extends Actor> actorClass() {
+        return TheActor.class;
+    }
+
+    final ActorRef myActor = getContext().actorOf(Props.create(DependencyInjector.class, applicationContext, "TheActor"), "TheActor");
+}
+
