63dcc4dccccdf3f77589d494bee593c8e0fc36a1
shmily <wangzaijun1234@126.com>
2021-09-01 23:10:01 +0800

    [add] akka : 学习如何在两个端收发数据

--- src/main/java/com/study/akka/myapp/server/HttpServer.java
+++ src/main/java/com/study/akka/myapp/server/HttpServer.java
@@ -2,20 +2,25 @@ package com.study.akka.myapp.server;
 
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.Behaviors;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.IncomingConnection;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.marshallers.jackson.Jackson;
-import akka.http.javadsl.model.HttpEntity;
-import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.model.*;
 import akka.http.javadsl.model.headers.Connection;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.http.javadsl.unmarshalling.Unmarshaller;
+import akka.japi.function.Function;
 import akka.stream.ActorMaterializer;
 import akka.stream.ActorMaterializerSettings;
 import akka.stream.IOResult;
+import akka.stream.Materializer;
 import akka.stream.javadsl.FileIO;
+import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
 import scala.concurrent.ExecutionContextExecutor;
 
 import java.io.File;
@@ -24,24 +29,40 @@ import java.util.concurrent.CompletionStage;
 public class HttpServer extends AllDirectives {
 
     public static void main(String[] args) throws Exception {
-        // boot up server using the route as defined below
-        ActorSystem<Void> system = ActorSystem.create(Behaviors.empty(), "routes");
+//        // boot up server using the route as defined below
+//        ActorSystem<Void> system = ActorSystem.create(Behaviors.empty(), "routes");
+//
+//        final Http http = Http.get(system);
+//
+//        //In order to access all directives we need an instance where the routes are define.
+//        HttpServer app = new HttpServer();
+//
+//        final CompletionStage<ServerBinding> binding =
+//                http.newServerAt("localhost", 8080)
+//                        .bind(app.createRoute());
+//
+//        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+//        System.in.read(); // let it run until user presses return
+//
+//        binding
+//                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+//                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+        akka.actor.ActorSystem system = akka.actor.ActorSystem.create();
+        Materializer materializer = ActorMaterializer.create(system);
 
-        final Http http = Http.get(system);
+        Source<IncomingConnection, CompletionStage<ServerBinding>> serverSource =
+                Http.get(system).bind(ConnectHttp.toHost("localhost", 80));
 
-        //In order to access all directives we need an instance where the routes are define.
-        HttpServer app = new HttpServer();
+        CompletionStage<ServerBinding> serverBindingFuture =
+                serverSource.to(Sink.foreach(connection -> {
+                            System.out.println("Accepted new connection from " + connection.remoteAddress());
+                            // ... and then actually handle the connection
+                        }
+                )).run(materializer);
 
-        final CompletionStage<ServerBinding> binding =
-                http.newServerAt("localhost", 8080)
-                        .bind(app.createRoute());
-
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read(); // let it run until user presses return
-
-        binding
-                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+        serverBindingFuture.whenCompleteAsync((binding, failure) -> {
+            // possibly report the failure somewhere...
+        }, system.dispatcher());
     }
 
     private Route createRoute() {
@@ -173,6 +194,57 @@ public class HttpServer extends AllDirectives {
         binding
                 .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
                 .thenAccept(unbound -> system.terminate()); // and shutdown when done
+
+    }
+
+    public void testFour() throws Exception{
+        final akka.actor.ActorSystem system = akka.actor.ActorSystem.create();
+        final Materializer materializer = ActorMaterializer.create(system);
+
+        Source<IncomingConnection, CompletionStage<ServerBinding>> serverSource = Http.get(system).bind(ConnectHttp.toHost("localhost",8080));
+
+        final Function<HttpRequest, HttpResponse> requestHandler = new Function<HttpRequest, HttpResponse>(){
+
+            private final HttpResponse NOT_FOUND = HttpResponse.create().withStatus(404).withEntity("Unknown resource");
+
+            @Override
+            public HttpResponse apply(HttpRequest httpRequest) {
+                Uri uri = httpRequest.getUri();
+                if (httpRequest.method() == HttpMethods.GET) {
+                    if (uri.path().equals("/")){
+                        return HttpResponse.create()
+                                .withEntity(ContentTypes.TEXT_HTML_UTF8, "<html><body>Hello world!</body></html>");
+                    } else if (uri.path().equals("/hello")){
+                        String name = uri.query().get("name").orElse("Mister X");
+                        return HttpResponse.create()
+                                .withEntity("Hello "+name+"!");
+                    } else if (uri.path().equals("/ping")){
+                        return HttpResponse.create().withEntity("PONG!");
+                    } else {
+                        return NOT_FOUND;
+                    }
+                } else {
+                    return NOT_FOUND;
+                }
+            }
+        };
+
+        CompletionStage<ServerBinding> serverBindingFuture =
+                serverSource.to(Sink.foreach(connection -> {
+                    System.out.println("Accepted new Connection from " + connection.remoteAddress());
+                    connection.handleWithSyncHandler(requestHandler, materializer);
+
+                    // this os equivalent to
+//                    connection.handleWith(Flow.of(HttpRequest.class).map(requestHandler), materializer);
+        })).run(materializer);
+
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read(); // let it run until user presses return
+
+        serverBindingFuture
+                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+
 
     }
 }
